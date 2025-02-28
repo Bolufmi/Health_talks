568db6698311964648c6ac021ec13402a42a6b94
Bolu'Fmi <boluwatifeolukayode@gmail.com>
2025-02-28 13:56:26 +0100

    There is a believe I have always had; I know things will always get better as long you don't give up

---
+++ Things will get better.php
@@ -0,0 +1,11 @@
+!DOCTYPE html
+<head>
+    <?php
+    echo "Things are going to get better, if you believe and put in the action needed"
+    ?>
+    <style>
+        *{
+            text-align: center;
+            color: Red;
+        }
+</head>
